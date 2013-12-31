e14b9c53422181062ad8986faea9a324c5fdb0be
sseppola <sseppola@me.com>
2013-12-31 15:09:49 +0100

    Modified bullet structure, game stopped working. I'll fix it soon if Vegard doesn't. Remind me to show you how git branches work.

--- canvas.js
+++ canvas.js
@@ -10,7 +10,6 @@ var yTiles = canvasHeight/squaresize;
 
 // Movement variables
 var dif = squaresize; // Stepsize
-var dir = "down"; // Initial direction
 
 // Game loop element
 var t;
@@ -23,22 +22,11 @@ randomizePosition = function(object) {
 	object.y = Math.floor(Math.random()*(yTiles-1))*squaresize;
 }
 
-var head = new Head();
-randomizePosition(head);
-
-fruit = new Object();
-fruit.move = randomizePosition;
-fruit.width = squaresize;
-fruit.height = squaresize;
-fruit.draw = function drawFruit() {
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(fruit.x, fruit.y, fruit.height, fruit.width);
-}
-fruit.move(fruit);
 
 function Head() {
 	this.width = squaresize;
 	this.height = squaresize;
+	this.dir = "down";
 	this.child = null;
 	this.shouldAddChild = false;
 
@@ -56,19 +44,19 @@ function Head() {
 		var prev_x = this.x,
 			prev_y = this.y;
 
-		if (dir == "left") {
+		if (head.dir == "left") {
 			this.x = this.x - dif;
 			if (this.x < 0)
 				this.x = 580;
-		} else if (dir == "up") {
+		} else if (head.dir == "up") {
 			this.y = this.y - dif;
 			if (this.y < 0)
 				this.y = 580;
-		} else if (dir == "right") {
+		} else if (head.dir == "right") {
 			this.x = this.x + dif;
 			if (this.x >= 600)
 				this.x = 0;
-		} else if (dir == "down") {
+		} else if (head.dir == "down") {
 			this.y = this.y + dif;
 			if (this.y >= 600)
 				this.y = 0;
@@ -94,6 +82,21 @@ function Head() {
 		}
 	}
 }
+
+var head = new Head();
+randomizePosition(head);
+
+fruit = new Object();
+fruit.move = randomizePosition;
+fruit.width = squaresize;
+fruit.height = squaresize;
+fruit.draw = function drawFruit() {
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(fruit.x, fruit.y, fruit.height, fruit.width);
+}
+fruit.move(fruit);
+
+
 
 function Child(parent) {
 	this.x = parent.x;
@@ -164,14 +167,28 @@ function updatePositions() {
 		head.addChild();
 		head.shouldAddChild = false;
 	}
-		head.update();	
+		// bulletBorderCheck();
+		head.update();
+
 }
 
 function redraw() {
 	ctx.clearRect(0,0,620,620);
 	fruit.draw();
 	head.draw();
+	if (bullet) {
+		bullet.draw();	
+	}
 }
+
+function bulletBorderCheck() {
+	if (bullet.x < 0 || bullet.x > canvasWidth - squaresize) {
+		bullet = null;
+	} else if (bullet.y < 0 || bullet.y > canvasHeight - squaresize) {
+		bullet = null;
+	}
+}
+
 function stopPlaying() {
 	console.log("stopping play");
 	clearInterval(t);
@@ -189,22 +206,25 @@ var command = {};
 command[80]  = togglePlay; // p key
 command[13]  = togglePlay; // Enter key
 command[37] = function () { // Left key
-	if(dir != "right")
-		dir = "left";
+	if(head.dir != "right")
+		head.dir = "left";
 }; 
 command[38] = function () { // Up key
-	if(dir !="down")
-		dir = "up";
+	if(head.dir !="down")
+		head.dir = "up";
 }; 
 command[39] = function () { // Right key
-	if(dir != "left")
-		dir = "right";
+	if(head.dir != "left")
+		head.dir = "right";
 }; 
 command[40] = function () { // Down key
-	if(dir != "up")
-		dir = "down";
+	if(head.dir != "up")
+		head.dir = "down";
 };
-command[32] = head.shoot;
+command[32] = function () {
+	console.log("pressed space");
+	head.shoot();
+}
 
 
 function togglePlay() {
@@ -232,33 +252,35 @@ document.addEventListener('keydown',function(event) {
 
 //Bullet shooting part of the game, not yet to be implemented
 
-var bullet = new Bullet();
+var bullet;
 
 head.shoot = function () {
-	bullet.drawBullet();
+	console.log("shooting");
+	bullet = new Bullet();
 }
 
 function Bullet() {
 	this.x = head.x;
 	this.y = head.y;
+	this.dir = head.dir;
 	this.width = squaresize/2;
 	this.height = squaresize/2;
 
-	this.drawBullet = function () {
+	this.draw = function () {
+		console.log("drawing bullet");
 		ctx.fillStyle = "#FF1500";
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
-}
-
-function moveBullet () {
-	if(dir=="right") {
-		bullet.x = bullet.x + 2*squaresize;
-	} else if (dir=="left") {
-		bullet.x = bullet.x - 2*squaresize;
-	} else if (dir=="up") {
-		bullet.y = bullet.y - 2*squaresize;
-	} else if (dir=="down") {
-		bullet.y = bullet.y + 2*squaresize;
+	this.move = function () {
+		if(bullet.dir=="right") {
+			bullet.x = bullet.x + 2*squaresize;
+		} else if (bullet.dir=="left") {
+			bullet.x = bullet.x - 2*squaresize;
+		} else if (bullet.dir=="up") {
+			bullet.y = bullet.y - 2*squaresize;
+		} else if (bullet.dir=="down") {
+			bullet.y = bullet.y + 2*squaresize;
+		}
 	}
 }
 
